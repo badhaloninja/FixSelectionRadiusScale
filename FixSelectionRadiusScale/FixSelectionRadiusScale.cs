1671070433aff7c69e302bef01fc02ef5f53aa42
badhaloninja <badhaloninja@badhalo.ninja>
2025-08-21 11:23:53 -0500

    Also scale the sphere collision check

--- FixSelectionRadiusScale/FixSelectionRadiusScale.cs
+++ FixSelectionRadiusScale/FixSelectionRadiusScale.cs
@@ -23,15 +23,17 @@ namespace FixSelectionRadiusScale
         [HarmonyPatch(typeof(DevTool))]
         class DevToolPatch
         {
-            static readonly MethodInfo slotGlobalPositionGetter = typeof(Slot).GetMethod("get_GlobalPosition", BindingFlags.Instance | BindingFlags.Public);
-            static readonly MethodInfo getScaledRadiusMethod = SymbolExtensions.GetMethodInfo(() => GetScaledRadius(null));
+            static readonly MethodInfo componentSlotGetter = typeof(Component).GetMethod("get_Slot", BindingFlags.Instance | BindingFlags.Public);
+            static readonly MethodInfo getScaledRadiusMethod = SymbolExtensions.GetMethodInfo(() => GetScaledRadius(0, null));
+
+            static readonly float COLLIDER_SEARCH_RADIUS = 0.025f;
 
             [HarmonyDebug]
             [HarmonyTranspiler]
             [HarmonyPatch("TryOpenGizmo")]
             public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                Msg("Slot.GlobalPosition.get: " + slotGlobalPositionGetter);
+                Msg("Slot.GlobalPosition.get: " + componentSlotGetter);
 
                 // Find local index for enumerated slot
                 //
@@ -39,67 +41,74 @@ namespace FixSelectionRadiusScale
                 //   load up our slot value onto the stack
                 //   call our get scale function with slot as the input and store onto the stack
 
-                // TODO Rework to fori loop
-                // TODO Compensate for sphere collision check as well
+                var foundSlotCheck = false;
+                var foundColliderCheck = false;
 
-                var found = false;
+                int? foundUserSlotLocal = null;
 
-                int previousLocal = -1;
-                bool foundSlotLocal = false;
-
-                OpCode previousOpcode = OpCodes.Nop;
+                CodeInstruction previousInstruction = null;
                 foreach (var instruction in instructions)
                 {
-                    // Find the local index for the curren't enumerated slot
-                    if (!foundSlotLocal)
+                    // Find the local index for the current enumerated slot
+                    if (!foundUserSlotLocal.HasValue)
                     {
                         // Store the last used local index
-                        if (instruction.IsLdloc())
-                        {
-                            previousLocal = instruction.LocalIndex();
-                        } 
-                        else if (instruction.Calls(slotGlobalPositionGetter))
+                        if (instruction.IsStloc() && previousInstruction.Calls(componentSlotGetter))
                         {
                             // Mark the previous local as our slot variable
-                            Msg("Found Slot.GlobalPosition.get the used local is: " + previousLocal);
-                            foundSlotLocal = true;
+                            foundUserSlotLocal = instruction.LocalIndex();
+                            Msg("Found Slot.GlobalPosition.get the used local is: " + foundUserSlotLocal.Value);
                         }
-                    }
-
-                    // Should have found the slot at this point
-                    // Check if we've found the correct 0.1f assignment
-                    if (foundSlotLocal && instruction.Is(OpCodes.Ldc_R4, 0.1f) && previousOpcode == OpCodes.Ldloc_S)
-                    {
-                        // Replace 0.1f with getScaledRadiusMethod(slot)
-
-                        // Load slot on to the stack
-                        yield return CodeInstruction.LoadLocal(previousLocal);
-                        // Load the value from call onto the stack
-                        yield return new CodeInstruction(OpCodes.Call, getScaledRadiusMethod);
-                        found = true;
-
-                        // Prevent writing 0.1f in the output
-                        continue;
                     }
 
                     // Write the original instruction
                     yield return instruction;
-                    // Store the previous instruction's opcode
-                    previousOpcode = instruction.opcode;
+
+                    // Should have found the slot at this point
+                    // Check if we've found the correct 0.1f assignment
+                    if (foundUserSlotLocal.HasValue && (!foundSlotCheck || !foundColliderCheck))
+                    {
+                        // Nearest slot radius
+                        if ((instruction.Is(OpCodes.Ldc_R4, DevTool.GIZMO_SEARCH_RADIUS) && previousInstruction.opcode == OpCodes.Ldloc_S) || instruction.Is(OpCodes.Ldc_R4, COLLIDER_SEARCH_RADIUS))
+                        {
+                            // Keep original value and let it be consumed as the first argument for GetScaledRadius(float, slot)
+
+                            // Load slot on to the stack
+                            yield return CodeInstruction.LoadLocal(foundUserSlotLocal.Value);
+                            // Load the value from call onto the stack
+                            yield return new CodeInstruction(OpCodes.Call, getScaledRadiusMethod);
+
+                            if (instruction.Is(OpCodes.Ldc_R4, COLLIDER_SEARCH_RADIUS)) 
+                                foundColliderCheck = true;
+                            else
+                                foundSlotCheck = true;
+                        }
+                    }
+
+                    previousInstruction = instruction;
                 }
-                if (found is false)
+
+                if (!(foundSlotCheck && foundColliderCheck))
                 {
                     Error("Cannot apply patch, something was not found!");
 
-                    if (foundSlotLocal)
+                    if (!foundUserSlotLocal.HasValue)
                     {
-                        Error("    Unable to find the slot local");
+                        Error("    Unable to find the user root slot local");
+                    }
+                    if (!foundSlotCheck)
+                    {
+                        Error("    Unable to find the slot radius check");
+                    }
+                    if (!foundColliderCheck)
+                    {
+                        Error("    Unable to find the collider radius check");
                     }
                 }
             }
 
-            public static float GetScaledRadius(Slot slot) =>
-                slot.LocalUserRoot.GlobalScale * DevTool.GIZMO_SEARCH_RADIUS;
+            public static float GetScaledRadius(float value, Slot slot) =>
+                slot.LocalUserRoot.GlobalScale * value; //DevTool.GIZMO_SEARCH_RADIUS;
 
 
         }
